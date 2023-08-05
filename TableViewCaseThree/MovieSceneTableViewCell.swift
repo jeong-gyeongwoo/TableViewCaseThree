5f78711008f718fc2060863491acea62ceecb0d5
gyeong <okbamtol@naver.com>
2023-08-05 11:58:00 +0900

    2주차 금과제 막힘

---
+++ TableViewCaseThree/MovieSceneTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieSceneTableViewCell.swift
+//  TableViewCaseThree
+//
+//  Created by 정경우 on 2023/08/04.
+//
+
+import UIKit
+
+class MovieSceneTableViewCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateTimeRateLabel: UILabel!
+    @IBOutlet var overviewLabel: UILabel!
+    @IBOutlet var movieImageLabel: UIImageView!
+    
+}
---
+++ TableViewCaseThree/MovieSceneTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MovieSceneTableViewController.swift
+//  TableViewCaseThree
+//
+//  Created by 정경우 on 2023/08/04.
+//
+
+import UIKit
+
+class MovieSceneTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib = UINib(nibName: "MovieSceneTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "MovieSceneTableViewCell")
+        // sizeCell()
+    }
+
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MovieInfo().movie.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //for 쓴거랑 안쓴거 차이 -> 요소만 수정? or 셀을 수정?
+        // 안해도 자동아닌가?
+        // xib 크기 조절 문제?
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieSceneTableViewCell", for: indexPath) as! MovieSceneTableViewCell
+        
+        cell.titleLabel.text = MovieInfo().movie[indexPath.row].title
+        cell.dateTimeRateLabel.text = " \(MovieInfo().movie[indexPath.row].releaseDate) | \(MovieInfo().movie[indexPath.row].runtime) | \(MovieInfo().movie[indexPath.row].rate) "
+        cell.overviewLabel.text = MovieInfo().movie[indexPath.row].overview
+        cell.movieImageLabel.image = UIImage(named: MovieInfo().movie[indexPath.row].title)
+        
+        return cell
+    
+    }
+   
+//    func sizeCell() {
+//        
+//        let layout = UITableViewCell()
+//        layout.sizeToFit()
+//
+//
+//
+//
+//    }
+
+}
